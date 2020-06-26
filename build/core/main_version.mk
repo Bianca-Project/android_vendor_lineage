c1fe21b8435d20dbd2e51d2f107316f0dba2eafd
alanndz <alanmahmud0@gmail.com>
2020-06-26 19:39:39 +0530

    Rebrand to Durandal Project
    Signed-off-by: alanndz <alanmahmud0@gmail.com>
    Change-Id: I13cc9f0fd85e627eeee718d8edd75313bb99a367

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,13 +4,12 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# DurandalProject System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.durandal.version=$(LINEAGE_VERSION) \
+    ro.durandal.releasetype=$(DURANDAL_BUILDTYPE) \
+    ro.durandal.build.version=v$(VERSION) \
+    ro.modversion=$(LINEAGE_VERSION)
 
 # LineageOS Platform Display Version
 ADDITIONAL_BUILD_PROPERTIES += \
